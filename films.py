85d9bc20121c2284874f10b5f8e9c6d66870dd87
dan_an <andrey.danilov@kryptonite.com>
2020-03-02 17:05:52 +0300

    refactored

--- films.py
+++ films.py
@@ -16,6 +16,7 @@ class Film():
   def get_movie_content(self):
     movie = Movie(self.id)
     movie.get_content('main_page')
+    print(movie.imdb_rating)
     self.plot = movie.plot
     self.genres = movie.genres
     self.rating = movie.rating
