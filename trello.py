41945bffeb427b9e83c13b92a1523c009ebe9968
dan_an <andrey.danilov@kryptonite.com>
2020-03-16 14:49:22 +0300

    WIP move film to watched list

--- trello.py
+++ trello.py
@@ -79,6 +79,7 @@ class Card:
         requests.post(f'{url}/cards', params=query)
 
     def move_card(self, card_id, list_id):
+        print('move')
         query = {
             **params,
             'idList': list_id,
