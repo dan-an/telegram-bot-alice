87773c0ec913e9b7c8684bf66837522d5a8f3fb2
dan_an <andrey.danilov@kryptonite.com>
2020-04-27 14:15:32 +0300

    [WIP] fix bug with IMDB - None

--- test.py
+++ test.py
@@ -2,11 +2,6 @@ import requests
 
 url = 'https://api.telegram.org/bot550506408:AAFS8jAyuapzVFsrlB9Gu1TWGO6T0BPFS4c/getUpdates'
 
-# proxies = {
-#     'http': 'socks5://telega:nNcl1BFJOG0@v1.gpform.pro:31080',
-#     'https': 'socks5://telega:nNcl1BFJOG0@v1.gpform.pro:31080'
-# }
-
 
 try:
     response = requests.get(url)
